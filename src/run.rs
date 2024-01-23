684c4746cf686ea6bfbbe8c6d8f1e49288bdc19e
nikodem <nikodemmarek11@gmail.com>
2024-01-23 10:55:02 +0000

    feat: recognise instructions

--- src/run.rs
+++ src/run.rs
@@ -12,8 +12,50 @@ pub fn run(file_path: &str) {
     while let instruction = hardware.next() {
         if instruction != 0b0000_0000_0000_0000 {
             println!("{:#06x}: {:#018b}", hardware.program_counter.get(), instruction);
+            process_instruction(instruction, &mut hardware);
         }
     }
+}
+
+fn process_instruction(instruction: u16, hardware: &mut Hardware) {
+    match instruction & 0b1111_0000_0000_0000 {
+        0b0001_0000_0000_0000 => {
+            if instruction & 0b0000_0000_0010_0000 == 0b0000_0000_0000_0000 {
+                // ADD 2 registers
+            } else {
+                // ADD register and imm5
+            }
+        }, // ADD
+        0b0101_0000_0000_0000 => {
+            if instruction & 0b0000_0000_0010_0000 == 0b0000_0000_0000_0000 {
+                // AND 2 registers
+            } else {
+                // AND register and imm5
+            }
+        }, // AND
+        0b0000_0000_0000_0000 => {}, // BR
+        0b1100_0000_0000_0000 => {}, // JMP / RET
+        0b1000_0000_0000_0000 => {
+            if instruction & 0b0000_1111_1111_1111 == 0b0000_0000_0000_0000 {
+                // RTI
+            } else if instruction & 0b0000_1000_0000_0000 == 0b0000_0000_0000_0000 {
+                // JSSR
+            } else {
+                // JSR
+            }
+        }, // JSR / JSRR / RTI
+        0b0010_0000_0000_0000 => {}, // LD
+        0b1010_0000_0000_0000 => {}, // LDI
+        0b0110_0000_0000_0000 => {}, // LDR
+        0b1110_0000_0000_0000 => {}, // LEA
+        0b1001_0000_0000_0000 => {}, // NOT
+        0b0011_0000_0000_0000 => {}, // ST
+        0b1011_0000_0000_0000 => {}, // STI
+        0b0111_0000_0000_0000 => {}, // STR
+        0b1111_0000_0000_0000 => {}, // TRAP
+        0b1101_0000_0000_0000 => {}, // reserved
+        _ => panic!("unrecognised instruction"),
+    };
 }
 
 fn read_binary_file(file_path: &str) -> io::Result<Vec<u16>> {
