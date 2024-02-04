c1a3535d93347cf6e2c0b898fb4fcde4b436a963
nikodem <nikodemmarek11@gmail.com>
2024-02-04 15:29:35 +0000

    feat: sign_extend function

--- src/utils.rs
+++ src/utils.rs
@@ -1,36 +1,23 @@
-pub fn imm5(value: u16) -> i16 {
-    let imm5 = value & 0b0000_0000_0011_1111;
-    if imm5 & 0b0000_0000_0001_0000 == 0b0000_0000_0000_0000 {
-        imm5 as i16
-    } else {
-        (imm5 | 0b1111_1111_1110_0000) as i16
+pub fn sign_extend(mut value: u16, bit_count: u8) -> i16 {
+    if (value >> (bit_count - 1)) & 1 != 0 {
+        value |= 0xFFFF << bit_count;
     }
+    value as i16
+}
+
+pub fn imm5(value: u16) -> i16 {
+    sign_extend(value & 0b0000_0000_0011_1111, 5) as i16
 }
 
 pub fn offset6(value: u16) -> i16 {
-    let offset6 = value & 0b0000_0000_0011_1111;
-    if offset6 & 0b0000_0000_0010_0000 == 0b0000_0000_0000_0000 {
-        offset6 as i16
-    } else {
-        (offset6 | 0b1111_1111_1100_0000) as i16
-    }
+    sign_extend(value & 0b0000_0000_0011_1111, 6) as i16
 }
 
 pub fn pcoffset9(value: u16) -> i16 {
-    let pcoffset9 = value & 0b0000_0001_1111_1111;
-    if pcoffset9 & 0b0000_0001_0000_0000 == 0b0000_0000_0000_0000 {
-        pcoffset9 as i16
-    } else {
-        (pcoffset9 | 0b1111_1110_0000_0000) as i16
-    }
+    sign_extend(value & 0b0000_0001_1111_1111, 9) as i16
 }
 pub fn pcoffset11(value: u16) -> i16 {
-    let pcoffset11 = value & 0b0000_0111_1111_1111;
-    if pcoffset11 & 0b0000_0100_0000_0000 == 0b0000_0000_0000_0000 {
-        pcoffset11 as i16
-    } else {
-        (pcoffset11 | 0b1111_1000_0000_0000) as i16
-    }
+    sign_extend(value & 0b0000_0111_1111_1111, 11) as i16
 }
 
 pub fn register_at(value: u16, at: u16) -> u16 {
@@ -42,31 +29,37 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_sign_extend() {
+        assert_eq!(sign_extend(0b0000_0000_0000_0001, 16), 0b0000_0000_0000_0001);
+        assert_eq!(sign_extend(0b0000_0000_0000_0001, 4), 0b0000_0000_0000_0001);
+    }
+
+    #[test]
     fn test_imm5() {
-        assert!(imm5(0b0000_0000_0000_0001) as u16 == 0b0000_0000_0000_0001);
-        assert!(imm5(0b0000_0000_0001_0001) as u16 == 0b1111_1111_1111_0001);
+        assert_eq!(imm5(0b0000_0000_0000_0001) as u16, 0b0000_0000_0000_0001);
+        assert_eq!(imm5(0b0000_0000_0001_0001) as u16, 0b1111_1111_1111_0001);
     }
 
     #[test]
     fn test_offset6() {
-        assert!(offset6(0b0000_0000_0000_0001) as u16 == 0b0000_0000_0000_0001);
-        assert!(offset6(0b0000_0000_0010_0001) as u16 == 0b1111_1111_1110_0001);
+        assert_eq!(offset6(0b0000_0000_0000_0001) as u16, 0b0000_0000_0000_0001);
+        assert_eq!(offset6(0b0000_0000_0010_0001) as u16, 0b1111_1111_1110_0001);
     }
 
     #[test]
     fn test_pcoffset9() {
-        assert!(pcoffset9(0b0000_0000_0000_0001) as u16 == 0b0000_0000_0000_0001);
-        assert!(pcoffset9(0b0000_0001_0000_0001) as u16 == 0b1111_1111_0000_0001);
+        assert_eq!(pcoffset9(0b0000_0000_0000_0001) as u16, 0b0000_0000_0000_0001);
+        assert_eq!(pcoffset9(0b0000_0001_0000_0001) as u16, 0b1111_1111_0000_0001);
     }
     #[test]
     fn test_pcoffset11() {
-        assert!(pcoffset11(0b0000_0000_0000_0001) as u16 == 0b0000_0000_0000_0001);
-        assert!(pcoffset11(0b0000_0100_0000_0001) as u16 == 0b1111_1100_0000_0001);
+        assert_eq!(pcoffset11(0b0000_0000_0000_0001) as u16, 0b0000_0000_0000_0001);
+        assert_eq!(pcoffset11(0b0000_0100_0000_0001) as u16, 0b1111_1100_0000_0001);
     }
 
     #[test]
     fn test_register_at() {
-        assert!(register_at(0b0000_1010_0000_0000, 9) == 0b0000_0000_0000_0101);
+        assert_eq!(register_at(0b0000_1010_0000_0000, 9), 0b0000_0000_0000_0101);
     }
 }
 
